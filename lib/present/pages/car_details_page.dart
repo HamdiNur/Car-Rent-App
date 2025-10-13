19140d5faa3586449b4a11ea099e476ad5232b48
HamdiNur <hamdoonur26@gmail.com>
2025-10-13 14:08:30 +0300

    Add homepage, onboarding, and Firebase setup

---
+++ lib/present/pages/car_details_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CardDetailsPage extends StatelessWidget {
+  const CardDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      
+    );
+  }
+}
---
+++ lib/present/pages/onboarding_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rentapp/present/pages/car_lis_screen.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff2C2B34),
+
+      body: Column(
+        children: [
+          // ✅ Top image area
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/onboarding.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          // ✅ Bottom text + button area
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Premium Car.\nEnjoy The Luxury',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Premium and prestige car daily rental.\nExperience the thrill at a lower price.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                  const SizedBox(height: 30),
+
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CarLisScree(),
+                          ),
+                        );
+                        // Add your navigation or logic here
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        // shape: RoundedRectangleBorder(
+                        //   borderRadius: BorderRadius.circular(12),
+                        // ),
+                        elevation: 3,
+                      ),
+                      child: const Text(
+                        "Let's Go",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
