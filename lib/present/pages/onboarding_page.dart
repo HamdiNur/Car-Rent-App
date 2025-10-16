b37ef9f09fe04b6602d2a9185b5f10ac0b54a0d4
HamdiNur <hamdoonur26@gmail.com>
2025-10-16 13:29:43 +0300

    Enhance onboarding_page: adjust design and layout for better user experience

--- lib/present/pages/onboarding_page.dart
+++ lib/present/pages/onboarding_page.dart
@@ -54,12 +54,11 @@ class OnboardingPage extends StatelessWidget {
                     height: 54,
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => CarLisScree(),
-                          ),
+                        Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(builder: (context)=>CarLisScree()),
+                          (route) => false,
                         );
+                       
                         // Add your navigation or logic here
                       },
                       style: ElevatedButton.styleFrom(
