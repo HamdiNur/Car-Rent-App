37770a743983ea36fcc4c1f4895f821b4c5eb08a
HamdiNur <hamdoonur26@gmail.com>
2025-10-25 21:14:34 +0300

    refactor: fix typo in Car.fromMap and prepare for clean architecture layering

--- lib/data/models/car.dart
+++ lib/data/models/car.dart
@@ -4,11 +4,29 @@ class Car {
   final double fuelCapacity;
   final double pricePerHour;
 
-
   Car({
-  required this.model, 
-  required this.distance, required this.fuelCapacity, required this.pricePerHour}
+    required this.model,
+    required this.distance,
+    required this.fuelCapacity,
+    required this.pricePerHour,
+  });
 
+  factory Car.fromMap(Map<String, dynamic> map) {
+    return Car(
+      model: map['model'],
+      distance: map['disntance'],
+      fuelCapacity: map['fuelCapacity'],
+      pricePerHour: map['pricePerHour'],
     );
-
+  }
 }
+
+///1.domain/entties
+///2.domain/repository(interface/contract)
+///3.domain/usecase /getCardById getCardByName
+
+
+
+///1.data/models
+///2.domain/datasource(fetch api/firebase)
+///3.domain/repositry
